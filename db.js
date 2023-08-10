bdfa5e944e4c3489a2a0b087fe2aa122ecd07c36
VasilenkoYurii <yura.vasilenko98@gmail.com>
2023-08-10 19:26:08 +0300

    fix db

--- db.js
+++ db.js
@@ -1,16 +1,5 @@
 const Pool = require("pg").Pool;
 
-// const pool = new Pool({
-//   user: "postgres",
-//   host: "localhost",
-//   post: 5452,
-//   database: "node_postgres",
-// });
-// Підключення до бази
-//  docker-compose up -d
-// psql -h localhost -p 5432 -U postgres -d mydb
-// password !23Awd
-
 const pool = new Pool({
   user: "postgres",
   host: "localhost",
